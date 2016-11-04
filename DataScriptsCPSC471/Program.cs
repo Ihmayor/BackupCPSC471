17f62d8e755cec94244a0673fa4f3eb4256ee7e3
Irene Mayor <ihmayor@ucalgary.ca>
2016-11-04 14:24:28 -0600

    I am getting through these. The only reason I'm writing this script is so we have flight between everything and generate times separated by every ten minutes for the today's current date for the next 24 hours. This way it can be re-run. THAT and it is a pain to manually update everything

--- DataScriptsCPSC471/Program.cs
+++ DataScriptsCPSC471/Program.cs
@@ -8,6 +8,24 @@ namespace DataScriptsCPSC471
 {
     public class Program
     {
+        static string SQLConnectionString { get { return string.Format("Server={0};Database={1};User Id={2};Password={3};", "136.159.7.84 ", "CPSC471_Fall2016_G7", "CPSC471_Fall2016_G7", "a\"-na9o$^`I&\"nw"); } }
+        public async static void ManuallyAddLayerInDatabase(string cityName, string airportName)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (DatabaseClassDataContext database = new DatabaseClassDataContext())
+                {
+                    DataScriptsCPSC471.AIRPORT layerToAdd = new DataScriptsCPSC471.AIRPORT()
+                    {
+                        CityName = cityName,
+                        Name= airportName
+                    };
+                    database.AIRPORTs.InsertOnSubmit(layerToAdd);
+                    database.SubmitChanges();
+                }
+            });
+        }
+
         static void Main(string[] args)
         {
         }
